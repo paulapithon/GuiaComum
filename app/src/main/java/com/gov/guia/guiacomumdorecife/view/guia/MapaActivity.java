a7f380e52ea0642d09283ff55740bf6c20e4bebe
Paula Pithon <paulapithon@gmail.com>
2017-11-14 19:47:47 -0300

    Criar diálogo do mapa

--- app/src/main/java/com/gov/guia/guiacomumdorecife/view/guia/MapaActivity.java
+++ app/src/main/java/com/gov/guia/guiacomumdorecife/view/guia/MapaActivity.java
@@ -2,11 +2,14 @@ package com.gov.guia.guiacomumdorecife.view.guia;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.database.ChildEventListener;
@@ -26,6 +29,16 @@ public class MapaActivity extends AppCompatActivity {
 
     @BindView(R.id.btns_mapa)
     RelativeLayout mBotoesMapa;
+    @BindView(R.id.mapa_popup)
+    View mMapaPopup;
+    @BindView(R.id.dialog_titulo)
+    TextView mTituloDialogo;
+    @BindView(R.id.dialogo_btn)
+    ImageButton mBtnDialogo;
+    @BindView(R.id.elevation_dialog)
+    View mElevation;
+
+    private Mapa currentBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +47,7 @@ public class MapaActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setupDatabase();
+        setUI();
     }
 
     private void setupDatabase() {
@@ -61,28 +75,16 @@ public class MapaActivity extends AppCompatActivity {
 
     private void criarBtnMapa (final Mapa btn, final String index) {
 
-        //TODO criar pasta com dimensões
-        //Seta os parametros do layout
-        RelativeLayout.LayoutParams parametros = new RelativeLayout.LayoutParams(200,200);
-        parametros.setMargins(
-                (int)(mBotoesMapa.getWidth() * btn.getX()),
-                (int)(mBotoesMapa.getHeight() * btn.getY()),
-                0,
-                0
-        );
-
         //Cria botao programaticamente
         ImageButton mapaBtn = new ImageButton(this);
         Glide.with(this).load(btn.getIcone()).into(mapaBtn);
-        mapaBtn.setLayoutParams(parametros);
+        mapaBtn.setLayoutParams(getParametrosLayout(btn, false));
         mapaBtn.setBackgroundColor(Color.TRANSPARENT);
         mapaBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO adicionar esse código dentro do diálogo
-                Intent intent = new Intent(MapaActivity.this, LivrosActivity.class);
+                criarDialogo(btn);
                 GuiaComumApplication.setsCurrentMap(index);
-                startActivity(intent);
             }
         });
         mBotoesMapa.addView(mapaBtn);
@@ -92,6 +94,67 @@ public class MapaActivity extends AppCompatActivity {
             GuiaComumApplication.getsBotoesMapa().put(index, btn);
         }
 
+    }
+
+    private void criarDialogo (Mapa btn) {
+
+        //Verificar se é botão atual para sumir com dialog
+        if (btn.equals(currentBtn)) {
+            mMapaPopup.setVisibility(View.GONE);
+            currentBtn = null;
+        } else {
+            mMapaPopup.setLayoutParams(getParametrosLayout(btn, true));
+            mMapaPopup.setVisibility(View.VISIBLE);
+            currentBtn = btn;
+        }
+
+        //Setar dialog com texto e listeners de botão atual
+        mTituloDialogo.setText(btn.getNome().toUpperCase());
+        mBtnDialogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MapaActivity.this, LivrosActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private RelativeLayout.LayoutParams getParametrosLayout (Mapa btn, boolean isDialog) {
+
+        //Tamanho da tela em dps
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        float logicalDensity = metrics.density;
+
+        double width = btn.getX();
+        double height = btn.getY();
+        int size = (int) Math.ceil(70 * logicalDensity);
+        if (isDialog) {
+            width -= 0.06;
+            height -= 0.15;
+            size = (int) Math.ceil(200 * logicalDensity);
+        }
+
+        RelativeLayout.LayoutParams parametros = new RelativeLayout.LayoutParams(size, size);
+        parametros.setMargins(
+                (int)(mBotoesMapa.getWidth() * width),
+                (int)(mBotoesMapa.getHeight() * height),
+                0,
+                0
+        );
+        return parametros;
+
+    }
+
+    private void setUI () {
+        mMapaPopup.bringToFront();
+
+        //Eleva dialog apenas se for uma versão suportada
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mElevation.setElevation(5f);
+            mElevation.setTranslationZ(5f);
+        }
     }
 
     @OnClick(R.id.btn_voltar)
